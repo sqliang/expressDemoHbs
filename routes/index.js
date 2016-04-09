ca3a3901fcbd03274e609d7cc931dfba02f7cb40
sqliang <1053196988@qq.com>
2016-04-09 16:11:15 +0800

    learning the hbs of node

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+/**
+ * index
+ */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'HomePage',
+    name: 'hello'
+  });
+});
+/**
+ * index
+ */
+router.get('/index',function(req,res,next){
+  res.render('index',{
+    title: 'HomePage',
+    name:'index'
+  });
+});
+/**
+ * login
+ */
+router.get('/login',function(req,res,next){
+  res.render('login',{
+    title: '登录',
+    username:'sqliang',
+    psw:'19891111',
+    layout: 'partials/login'
+  });
 });
 
 module.exports = router;
